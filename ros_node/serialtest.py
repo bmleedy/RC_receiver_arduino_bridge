0f22583bc8a3f525433c83e227becf22448ce143
Leedy <leebrett@amazon.com>
2018-03-07 19:29:08 -0800

    Added parsing and formatted output.

--- ros_node/serialtest.py
+++ ros_node/serialtest.py
@@ -1,8 +1,19 @@
 import serial
 import io
 
-ser = serial.Serial('COM4', 9600, timeout=1)
+ser = serial.Serial('/dev/tty.SLAB_USBtoUART', 9600, timeout=1)
 
 while True:
   line = ser.readline()
-  print(line)
+  values = line.split(",")
+
+
+  fields = []
+  for value in values:
+    fields.append(int(value))
+
+
+  # Just output the values for debug purposes
+  if len(fields) > 4:
+    print "Time(ms): ", repr(fields[0]).ljust(10), repr(fields[1]).rjust(5), repr(fields[2]).rjust(5), repr(fields[3]).rjust(5),repr(fields[4]).rjust(5)
+
